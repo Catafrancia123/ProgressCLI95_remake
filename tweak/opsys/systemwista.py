06ec2836e5c2a5968e38a64b2db72bac4e9273a1
normie <aragonisafferz@gmail.com>
2023-03-18 16:43:31 +0700

    Delete tweak directory

--- tweak/opsys/systemwista.py
+++
@@ -1,8 +0,0 @@
-class system():
-    name = "Progressbar Wista"
-    shortname = "wista"
-    prolevel = 40
-    unlocklevel = 45
-    requiredstring = "PBXB"
-    listinbootmenu = 6
-    startupstring = "W i s t a"
