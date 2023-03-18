a947222e9c1274f86eef3ac42e22cfb91443df5f
normie <aragonisafferz@gmail.com>
2023-03-18 16:40:15 +0700

    the new files

---
+++ code/lang/sw_SE.py
@@ -0,0 +1,63 @@
+class language():
+  # se_SE language configuration file
+  # {*} <-- these are for python's .format() command, used for placing variables in strings.
+  # Please do not remove \n, {*}, or [] objects.
+
+  # boot
+  pleaseSelect = 'Välj språk med en kod på vänster sida.'
+  sparrow = '[white]Sparrow Assistant Enhanced Text BIOS.[not bold]80.1[/not bold][/white] - [bright_yellow]Energy Star (un)Powered[/bright_yellow]'
+  # {0} is for the build verson, {1} is for the compile date.
+  version = '[white]CLI ver. [bold]{0}[/bold] - compiled {1}[/white]'
+  dev = '[bold red]- DEVELOPMENT BUILD // BUGS MAY BE COMMON -[/bold red]\n\n'
+  # {0} is for the boot menu list number, {1} is for the full system name, {2} is the level number required for unlock, {3} is the level you need in a specified system to unlock this one.
+  notUnlocked = '[red][not bold]{0}[/not bold]. [not bold]{1}[/not bold] - Du behöver level {2} in {3} för att låsa upp det![/red]'
+  credits1 = "Progress[#12cc00]CLI[/#12cc00]95 [blink]Produktion[/blink] och [blink]Medarbetar[/blink] Lag"
+  credits2 = "Progress[#12cc00]CLI[/#12cc00]95 [blink]Översättnings[/blink] Lag"
+
+  # player
+  loading = '\n\n\nLaddar...'
+  bar = '\nDin progressbar:'
+  barProgress1 = '\nDu har {0}% med {1}% orange i din progressbar.'
+  barProgress2 = "\nDu har {0}% i din progressbar."
+  doYouWant = "Vill du ha det :"
+  popupSetting = "\nvill du ha det till ditt popup fönster? (J/N)"
+  wait = 'Ett ögonblick . . .\n\n\n'
+  closeCMD = '[bold yellow]Det är säkert att stänga ditt Command Line Interface nu.[/bold yellow]'
+  bm1 = 'M e n u'
+  bm2 = 'Nytt spel'
+  bm3 = 'Ladda Spel'
+  bm4 = 'Omstart'
+  bm5 = 'Stäng ner'
+  bm6 = 'Inställningar'
+  bm7 = 'Återgå'
+  sett1 = 'Inställningar'
+  sett2 = 'Popup-fönster'
+  sett3 = 'Återgå'
+  annoyingPopup = ':) Jobbig popup!'
+  outOfLives = "[bold bright_blue]Du har slut på liv. Spelet är slut![/bold bright_blue]"
+  noLevelTaken = '[i]Du har inte tagit en level.[/i]'
+  negateLevel = '[bold i]-1 Level[/bold i]'
+  level = 'Level'
+  oneLifeLeft = "Du har [italic bright_red]1 liv kvar[/italic bright_red]. Var Försiktig."
+  # {0} is the current number of lives.
+  livesLeft = "Du har {0} liv kvar."
+  pressInstructions = "Tryck 'c' för att fånga, 'q' för att avsluta och vilken annan tangent som helst för att flytta dig.\n"
+  gameOver = 'Spelet är slut! Tack för att du spelade!'
+  pressEnter = '\nTryck ENTER för att spela.'
+  gameBravo = 'Bravo!'
+  gamePerfect = 'Perfekt!'
+  gameOuterSpace = 'Yttre Rymden!'
+  gameYinAndYang = 'Yin och yang'
+  gameNonconformist = "Icke konformistisk!"
+  gamePoints = " poäng"
+  newSystem = '[i]Du har låst upp ett nytt system...[/i]'
+  proCongrats = '\nGrattis! Du är nu Professional!'
+  proAcquired = 'Du uppnådde Pro Titel!'
+  expertAcquired = '\nDu uppnådde Expert Titel!'
+  masterAcquired = '\nDu uppnådde Mästar Titel!'
+  adeptAcquired = '\nDu uppnådde Erfaren Titel!'
+  grandAcquired = '\nDu uppnådde Storslagen Titel!'
+  grandmaster = "\nDu uppnådde Stormästare Titel!"
+  discordmod = "\nRör du ens gräs?"
+  whatAcquired = '\nVa?'
+  magicPink = 'Magisk Rosa!'
---
+++ code/opsys/system2000.py
@@ -0,0 +1,9 @@
+class system():
+    name = "Progressbar 2000"
+    shortname = "2000"
+    prolevel = 30
+    unlocklevel = 35
+    systemunlock = "xb"
+    requiredstring = "PBMeme"
+    listinbootmenu = 4
+    startupstring = "2 0 0 0"
