6c73c309e109107b60db20dc25086fabe1d58f7a
normie <aragonisafferz@gmail.com>
2024-08-11 22:54:49 +0700

    Delete post directory

--- post/clear.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys as system
-
-def clear():
-    if system.platform.startswith(('win32')):
-        os.system('cls')
-    elif system.platform.startswith(('linux', 'cygwin', 'darwin', 'freebsd')):
-        os.system('clear')
-
--- post/opsys/system95plus.py
+++
@@ -1,9 +0,0 @@
-class system():
-    name = "Progressbar 95 plus"
-    shortname = "95plus"
-    prolevel = 20
-    unlocklevel = 15
-    systemunlock = "98"
-    requiredstring = "PB95"
-    listinbootmenu = 1
-    startupstring = "9 5  p l u s"
